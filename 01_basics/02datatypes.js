719003ee96decc2e2b8d740a19600b9f99e8f91e
bhutuklearning <139222826+bhutuklearning@users.noreply.github.com>
2023-07-11 16:34:00 +0000

    Datatypes

---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,18 @@
+//Simple idea about scope ,i.e, something inside the curly braces {}
+"use strict"; //treat all Js code as newer version
+
+//alert(3+3) // we are using node js not browser.
+//Code readability should be high
+
+let name = "Bhutuk";
+let age = 19;
+let isLoggedIn = false;
+
+//number is a datatype of range => 2 to power 53
+//bigint, string, boolean(true/false),null(standalone), undefined(value is not assigned),symbol(unique) are also datatype
+//Object is a datatype.
+console.log(typeof "Bhutuk");
+console.log(typeof true);
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
