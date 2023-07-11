7a978b5b1cdc9cb34bb209eb525539948b02f96d
bhutuklearning <139222826+bhutuklearning@users.noreply.github.com>
2023-07-11 12:39:01 +0000

    corrected

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -4,4 +4,4 @@ let emailId = "email@email.com";
 let password = "448271";
 let accountCity = "Delhi";
 console.table([accountId,emailId,password,accountCity]);
-//Here I got to know about console.table and its usecase. Lookin forward to progress a lot.
+//Here I got to know about console.table and its usecase. Looking forward to progress a lot.
