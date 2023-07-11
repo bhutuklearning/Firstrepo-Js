b9299529f215b2940a619db97e655cfeca695363
bhutuklearning <139222826+bhutuklearning@users.noreply.github.com>
2023-07-11 16:08:05 +0000

    Updated the file name.

---
+++ 01_basics/01variables.js
@@ -0,0 +1,7 @@
+console.log("Hello, World");
+let accountId = 576224;
+let emailId = "email@email.com";
+let password = "448271";
+let accountCity = "Delhi";
+console.table([accountId,emailId,password,accountCity]);
+//Here I got to know about console.table and its usecase. Looking forward to progress a lot.
