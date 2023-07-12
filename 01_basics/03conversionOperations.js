1d958d885e39a0f48517f9bf5de849035b2282da
bhutuklearning <139222826+bhutuklearning@users.noreply.github.com>
2023-07-12 07:33:51 +0000

    Learned about operations

--- 01_basics/03conversionOperations.js
+++ 01_basics/03conversionOperations.js
@@ -28,3 +28,38 @@ let someNumber = 44;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ****Operations*****
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.table([2+2, 2-2, 2*2, 2**3, 2/2, 2%2]);//2**3:- 2 to the power 3
+
+let str1 = "Hello";
+let str2 = " Bhutuk";
+let str3 = str1 + str2 ;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+"2");
+console.log("1"+2+2);//string pehle hai to pehle hi string mein convert kar diya jayega for reference go to ECMA script.
+console.log(1+2+"2");
+
+//Don't write confusing as well as messy codes.
+console.log( (3+4)*5 % 3);
+console.log(true, +true, +"");// not recommended
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2;//not recommended 
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+//go and search about prefix and postfix js mdn
