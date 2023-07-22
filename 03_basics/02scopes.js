9bb491f3ce1a3a7f4108b8f0a6acc505785a3dbc
bhutuklearning <139222826+bhutuklearning@users.noreply.github.com>
2023-07-22 13:41:35 +0000

    Nested scope and mini hoisting.

--- 03_basics/02scopes.js
+++ 03_basics/02scopes.js
@@ -17,3 +17,45 @@ if(true) {
     console.log("Inner: ",x);
 }
 console.log(x);
+
+//+++Nested Scope++
+
+function one() {
+    const username = "Bhutuk";
+
+    function two() {
+        const website = "Youtube";
+        console.log(username);
+    }
+    // console.log(website);
+
+    two();
+}
+
+one();
+
+if(true) {
+    const username = "bhutuk";
+    if(username === "bhutuk") {
+        const website = " youtube";
+        console.log(username+website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+//+++++++++ interesting +++++++
+console.log(addOne(11)); //hoisting
+
+function addOne(num) {
+    return num+1;
+}
+
+const addTwo = function(num) { //it's called expression
+    return num+2;
+}
+
+console.log(addTwo(12));
+
+//hoisting is done for function call but not for an expression,i.e, a variable which holds the value of the function.
