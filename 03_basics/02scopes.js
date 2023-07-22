46ce598a7b9851e4aac85d247a432d2cc0e53d6a
bhutuklearning <139222826+bhutuklearning@users.noreply.github.com>
2023-07-22 13:10:32 +0000

    Introduction to Scopes.

---
+++ 03_basics/02scopes.js
@@ -0,0 +1,19 @@
+var c =300;
+
+if(true) {
+    let a =10;
+    const b= 20;
+    var c = 30;
+}
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+let x = 112;
+
+if(true) {
+    let x = 10;
+    const y = 1024;
+    console.log("Inner: ",x);
+}
+console.log(x);
