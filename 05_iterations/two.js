57090a10d94d35ca341adda17cf9136a43b7e340
bhutuklearning <139222826+bhutuklearning@users.noreply.github.com>
2023-07-25 19:26:39 +0000

    While and do-while loops basics.

---
+++ 05_iterations/two.js
@@ -0,0 +1,33 @@
+//While loop
+
+let u = 1;
+while(u<=10) {
+    console.log(`Value of i is ${u}`);
+    u=u+2;
+}
+
+let myArray = ["Batman", "Superman", "Flash"];
+
+let arr = 0;
+while(arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+}
+
+//Do-while loop
+
+let score= 1;
+do {
+    console.log(`Score is ${score}.`);
+    score++;
+} while(score<=10);
+
+console.log("Special property of Do-while.");
+let score1 = 11;
+do{
+    console.log(`Score is ${score1}.`);
+    score1++;
+}while(score1<=10);
+
+
+
